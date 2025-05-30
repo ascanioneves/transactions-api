f3b630e9f5ccd48403ca9c3a366a925f6a5ee5f5
ascanioneves <ascaniosavio97@gmail.com>
2025-05-29 22:33:41 -0300

    feat: finish transactions api

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import fastify from 'fastify'
+import { knex } from './database'
+import { randomUUID } from 'node:crypto'
+import { env } from './env'
+import { transactionRoutes } from './routes/transactions'
+import cookie from '@fastify/cookie'
+
+const app = fastify()
+
+app.register(cookie)
+app.register(transactionRoutes, {
+  prefix: 'transactions',
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
