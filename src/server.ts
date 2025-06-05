fa22091561448d474c118dfe9f6cf7e3d206ccf2
ascanioneves <ascaniosavio97@gmail.com>
2025-06-04 21:27:32 -0300

    feat: adding tests

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,5 @@
-import fastify from 'fastify'
-import { knex } from './database'
-import { randomUUID } from 'node:crypto'
-import { env } from './env'
-import { transactionRoutes } from './routes/transactions'
-import cookie from '@fastify/cookie'
-
-const app = fastify()
-
-app.register(cookie)
-app.register(transactionRoutes, {
-  prefix: 'transactions',
-})
+import { app } from "./app"
+import { env } from "./env"
 
 app
   .listen({
